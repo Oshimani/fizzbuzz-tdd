be698c9c84b6beb5018d48ce295ba012d8a5dc02
Jannick Jungbluth <jannick.jungbluth@moccamedia.com>
2024-12-17 15:45:10 +0100

    init

---
+++ fizzbuzz.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import { fizzBuzz } from "./index";
+
+describe("fizzbuzz", () => {
+  const inputArray = Array.from(Array(100).keys());
+
+  it("should output the numbers of the array one by one", () => {});
+
+  it('should output "Fizz", when the number is divisible by 3', () => {});
+
+  it('should output "Buzz", wenn the number is divisible by 5', () => {});
+
+  it('should output "FizzBuzz", when the number is divisible by 3 and 5', () => {});
+});
---
+++ index.ts
@@ -0,0 +1,1 @@
+export function fizzBuzz(array: number[]) {}
